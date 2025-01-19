9556cdab479029a472e6948b40be2e308991987e
FredyRosero <desarrollo3@experis.co>
2025-01-18 23:30:19 -0500

    Reset bird to slingshot

--- angry-birds/sketch.js
+++ angry-birds/sketch.js
@@ -1,92 +1,239 @@
-const {Engine, World, Bodies,
-  Mouse, MouseConstraint,
-  Body, Constraint, Events
-} = Matter;
+/*
+# "Angry Birds" assignment
 
-let engine, world, ground,
-boxes = [], boxImg, groundImg,
-bird, birdImg = [], slingshot,
+## Description
+Implement Angry Birds using a Physics Engine
+* Design a complete level.
+* Add pigs with textures.
+* Add a background.
+* Add a texture to the slingshot.
+* Define a lifetime for the objects and enemies.
+
+*/
+
+const { Engine, World, Bodies, Mouse, MouseConstraint, Body, Constraint, Events } = Matter;
+
+let 
+engine, 
+world, 
+ground, 
+boxes = [], 
+boxImg, 
+groundImg, 
+bird, 
+birdImg = [], 
+slingshot, 
 mc;
 
-function setup(){
-const canvas = 
-  createCanvas(500,300);
+function setup() {
+  const canvas =
+    createCanvas(500, 300);
 
-boxImg = loadImage("img/box.png");
-groundImg = loadImage("img/ground.jpg");
+  boxImg = loadImage("img/box.png");
+  groundImg = loadImage("img/ground.jpg");
 
-birdImg = [
-  loadImage("img/red.png"),
-  loadImage("img/yellow.png"),
-] 
+  birdImg = [
+    loadImage("img/red.png"),
+    loadImage("img/yellow.png"),
+  ]
 
-engine = Engine.create();
-world = engine.world;
+  engine = Engine.create();
+  world = engine.world;
 
-const mouse = Mouse.create(canvas.elt);
-mouse.pixelRatio = pixelDensity();
-mc =
-  MouseConstraint.create(engine,
-  {
-    mouse: mouse,
-    collisionFilter: {mask: 2}
-  });
-World.add(world, mc);
+  const mouse = Mouse.create(canvas.elt);
+  mouse.pixelRatio = pixelDensity();
+  mc =
+    MouseConstraint.create(engine,
+      {
+        mouse: mouse,
+        collisionFilter: { mask: 2 }
+      });
+  World.add(world, mc);
 
-ground = new Ground(
-  width/2, height-10,
-  width, 20, groundImg
-);
-
-for (let i=0; i<=6; i++) {
-  let box = new Box(
-    400, height - 40*i,
-    40, 40, boxImg
+  ground = new Ground(
+    width / 2, height - 10,
+    width, 20, groundImg
   );
-  boxes.push(box);
-  
-  box = new Box(
-    440, height - 40*i,
-    40, 40, boxImg
-  );
-  boxes.push(box);
+
+  for (let i = 0; i <= 6; i++) {
+    let box = new Box(
+      400, height - 40 * i,
+      40, 40, boxImg
+    );
+    boxes.push(box);
+
+    box = new Box(
+      440, height - 40 * i,
+      40, 40, boxImg
+    );
+    boxes.push(box);
+  }
+
+  bird = new Bird(100, 200, 15, birdImg[0]);
+  slingshot = new SlingShot(bird);
 }
 
-bird = new Bird(
-  100, 200, 15, birdImg[0]);
-  
-slingshot = new SlingShot(
-  bird);
-  
-/*Events.on(engine,
-  'afterUpdate',
-  () => slingshot.fly(mc)
-  );*/
+function draw() {
+  background("#C6E7FF");
+  Engine.update(engine);
+  slingshot.fly(mc);
+  bird.update();
+
+  for (const box of boxes) {
+    box.show();
+  }
+
+  slingshot.show();
+  bird.show();
+  ground.show();
 }
 
-function draw(){
-background("#C6E7FF");
-Engine.update(engine);
-slingshot.fly(mc);
-
-for (const box of boxes){
-  box.show();
+function keyPressed() {
+  if (key == ' ') {
+    bird.clear();
+    const index = floor(random(0, birdImg.length));
+    bird = new Bird(100, 200, 15, birdImg[index]);
+    slingshot.attach(bird);
+  }
 }
 
-slingshot.show();
-bird.show();  
-ground.show();
+class Bird {
+  constructor(x, y, r, img) {
+    this.body = Bodies.circle(
+      x, y, r, // set x and y position and radius
+      {
+        restitution: 0.5, // bounciness
+        collisionFilter: { category: 2 } // set collision category to 2
+      }
+    );
+    Body.setMass(this.body, 2);
+    this.img = img;
+    World.add(world, this.body);
+    this.isFlying = false;
+    this.timeToLiveAfterCollision = 100;
+  }
+
+  show() {
+    push();
+    if (this.img) {
+      imageMode(CENTER);
+      translate(this.body.position.x, this.body.position.y);
+      rotate(this.body.angle);
+      image(this.img, 0, 0, 2 * this.body.circleRadius, 2 * this.body.circleRadius);
+    } else {
+      ellipse(this.body.position.x, this.body.position.y, 2 * this.body.circleRadius, 2 * this.body.circleRadius);
+    }
+    pop();
+  }
+
+  clear() {
+    bird = new Bird(100, 200, 15, birdImg[0]);
+    slingshot.setSling(bird);
+  }
+
+  update() {    
+    if (this.isFlying) {
+      this.timeToLiveAfterCollision--;
+      if (this.timeToLiveAfterCollision <= 0) {
+        this.clear();
+      }
+    }
+  }
 }
 
-function keyPressed(){
-if (key== ' ') {
-  bird.clear();
+class Box {
+  constructor(x, y, w, h, img, options = {}) {
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    this.img = img;
+    World.add(world, this.body);
+  }
+
+  show() {
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(this.body.angle);
+
+    if (this.img) {
+      imageMode(CENTER);
+      image(this.img, 0, 0, this.w, this.h);
+    } else {
+      rectMode(CENTER);
+      rect(0, 0, this.w, this.h);
+    }
+    pop();
+  }
+
+}
+
+class Ground extends Box {
+  constructor(x, y, w, h, img) {
+    super(x, y, w, h, img,
+      { isStatic: true });
+  }
+}
+
+class SlingShot {
+  constructor(bird) {
+    this.sling = Constraint.create({
+      pointA: {
+        x: bird.body.position.x,
+        y: bird.body.position.y
+      },
+      bodyB: bird.body,
+      length: 5,
+      stiffness: 0.01
+    });
+    World.add(world, this.sling);
+  }
   
-  const index =
-    floor(random(0, birdImg.length));
-  
-  bird = new Bird(
-  100, 200, 15, birdImg[index]);
-  slingshot.attach(bird);
-}
+  setSling(bird) {
+    this.sling.pointA = {
+      x: bird.body.position.x,
+      y: bird.body.position.y
+    };
+    this.sling.bodyB = bird.body;
+  }
+
+  show() {
+    if (this.sling.bodyB) {
+      line(this.sling.pointA.x,
+        this.sling.pointA.y,
+        this.sling.bodyB.position.x,
+        this.sling.bodyB.position.y);
+    }
+  }
+
+  /**
+   * Mechanism to release the bird and make it fly
+   * 
+   * @param {*} mc 
+   */
+  fly(mc) {
+    // If the bodyB is not null and the mouse button is released AND the bodyB is to the right of the slingshot
+    if (this.sling.bodyB && mc.mouse.button === -1 && (this.sling.bodyB.position.x > this.sling.pointA.x + 10)) {
+      // now the bodyB can collide with other bodies in the category 1
+      this.sling.bodyB.collisionFilter.category = 1;
+      bird.isFlying = true;
+      // detach the body
+      this.detach();
+    }
+  }
+
+  /**
+   * Detach the bird from the slingshot
+   */
+  detach() {
+    this.sling.bodyB = null;
+  }
+
+  /**
+   * Attach the bird to the slingshot
+   * 
+   * @param {*} bird 
+   */
+  attach(bird) {
+    this.sling.bodyB = bird.body;
+  }
 }
